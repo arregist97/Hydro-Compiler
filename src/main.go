30f2a890ff38059c691fedfc2bf81650404bec22
Arthur <arregist@gmail.com>
2024-07-21 10:26:24 -0700

    Added recursive tokenization function, fixed bugs.

--- src/main.go
+++ src/main.go
@@ -49,6 +49,9 @@ func main() {
 }
 
 func tokenize(content string) []string {
+	var empty []string
+	test := recTokenize(content, empty)
+	fmt.Println(test)
 	re := regexp.MustCompile(`([\s()])`)
 
 	content = re.ReplaceAllStringFunc(content, func(s string) string {
@@ -59,29 +62,73 @@ func tokenize(content string) []string {
 	})
 
 	parsedContent := strings.Fields(content)
+	fmt.Println(parsedContent)
 
 	return parsedContent
 }
 
-func buildToken(content string, iOpt ...uint8) (string, string) {
+func recTokenize(content string, tokens []string) []string {
+	var token string
+	var updatedContent string
+	var err error
+
+	token, updatedContent, err = buildToken(content)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+	fmt.Println(token + "/end")
+	if len(token) > 0 {
+		tokens = append(tokens, token)
+	}
+	if len(content) > 0 {
+		return recTokenize(updatedContent, tokens)
+	}
+	return tokens
+}
+
+func buildToken(content string, iOpt ...uint8) (string, string, error) {
 	var updatedToken string
 	var updatedContent string
-	i := 0
+	var err error = nil
+	var i uint8 = 0
+
 	if len(iOpt) > 0 {
 		i = iOpt[0]
 	}
-	r, size := utf8.DecodeRuneInString(content[i:])
 
-	if r == " " && i == 0 {
-		updatedToken, updatedContent = buildToken(content[size:])
-	} else if r == "(" || r == ")" || r == " " {
+	if uint8(len(content)) <= i {
+		return "", "", err
+	}
+
+	r, s := utf8.DecodeRuneInString(content[i:])
+	if r == utf8.RuneError {
+		return "", "", errors.New("Could not recognize token " + content[i:i+1]) 
+	}
+
+	size := uint8(s)
+	peek, _ := utf8.DecodeRuneInString(content[i+size:])
+
+	if r == ' ' && i == 0 {
+		updatedToken, updatedContent, err = buildToken(content[size:])
+	} else if isEndOfToken(r) || peek == utf8.RuneError || isEndOfToken(peek) {
 		updatedToken = string(r)
 		updatedContent = content[i + size:]
 	} else {
-		token, updatedContent := buildToken(content, i + size)
+		var token string
+		token, updatedContent, err = buildToken(content, i + size)
 		updatedToken = string(r) + token
 	}
-	return updatedToken, updatedContent  
+	return updatedToken, updatedContent, err
+}
+
+func isEndOfToken(a rune) bool {
+	var endOfTokenRunes = [...]rune {'(', ')', ' ', '\n'}
+	for _, b := range endOfTokenRunes {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 func parse(tokens []string) (string, error){
