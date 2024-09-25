93795e309e40dd6124df6d3d723eb94659edd3cc
Arthur <arregist@gmail.com>
2024-09-24 21:09:43 -0700

    Added Comments

--- src/tokenizer/tokenizer.go
+++ src/tokenizer/tokenizer.go
@@ -53,18 +53,71 @@ func buildToken(content string, iOpt ...uint8) (string, string, error) {
 
 	size := uint8(s)
 	peek, _ := utf8.DecodeRuneInString(content[i+size:])
-
-	if r == ' ' && i == 0 {
-		updatedToken, updatedContent, err = buildToken(content[size:])
-	} else if isEndOfToken(r) || peek == utf8.RuneError || isEndOfToken(peek) {
+	if peek == utf8.RuneError {
 		updatedToken = string(r)
 		updatedContent = content[i + size:]
+		return updatedToken, updatedContent, err
+	}
+	if r == ' ' && i == 0 {
+		updatedToken, updatedContent, err = buildToken(content[size:])
+	} else if r == '/' && (peek == '/' || peek == '*') {
+		lnCmt := false
+		if peek == '/' {
+			lnCmt = true
+		}
+		updatedToken = ""
+		updatedContent, err = skipComment(content, i + 2, lnCmt)
+	} else if isEndOfToken(r)  || isEndOfToken(peek) {
+		updatedToken = string(r)
+		updatedContent = content[i+size:]
 	} else {
 		var token string
 		token, updatedContent, err = buildToken(content, i + size)
 		updatedToken = string(r) + token
 	}
 	return updatedToken, updatedContent, err
+}
+
+func skipComment(content string, i uint8, lineComment bool) (string, error) {
+	r, s := utf8.DecodeRuneInString(content[i:])
+	if r == utf8.RuneError {
+		if s == 1 {
+			return "", errors.New("Could not recognize token " + content[i:i+1]) 
+		} else {
+			return "", errors.New("Empty decode error")
+		}
+	}
+
+	size := uint8(s)
+	
+	if lineComment && r == '\n' {
+		updatedContent := content[i:]
+		return updatedContent, nil
+	}
+
+	if r != '*' || lineComment {
+		return skipComment(content, i + 1, lineComment)
+	}
+	
+	var peek rune
+	peek, s = utf8.DecodeRuneInString(content[i+size:])
+	if peek == utf8.RuneError {
+		if s == 1 {
+			return "", errors.New("Could not recognize token " + content[i:i+1]) 
+		} else {
+			return "", errors.New("Empty decode error")
+		}
+	}
+
+	size = size + uint8(s)
+	if peek == '/' {
+		updatedContent := content[i+size:]
+		return updatedContent, nil
+	}
+
+
+	return skipComment(content, i + 1, lineComment)
+
 }
 
 func isEndOfToken(a rune) bool {
